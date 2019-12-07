9b5468190eaf401a0e9f80bae21e83040cf8ba4f
zoe <904954274@qq.com>
2019-12-07 18:40:06 +0800

    迁移至androidX

--- app/src/main/java/com/zoe/example/kydialog/MainActivity.java
+++ app/src/main/java/com/zoe/example/kydialog/MainActivity.java
@@ -1,20 +1,11 @@
 package com.zoe.example.kydialog;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 
-import com.zoe.kydialog.KyDialog;
 import com.zoe.kydialog.briefdialog.BriefDialog;
-import com.zoe.kydialog.briefdialog.SimpleListDialog;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
